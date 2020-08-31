444be05eeba26a5ad794dd91ee087e54ab8e8793
James Bacon <james@baconi.co.uk>
2020-08-31 12:49:55 +0100

    Basic attempt at parsing input for multiple commands with optional options.

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,9 @@ dependencies {
     // Logging
     implementation("ch.qos.logback:logback-classic:1.2.3")
 
+    // CLIKT - Command line helper
+    implementation("com.github.ajalt.clikt:clikt:3.0.0-rc")
+
     // Test framework
     testImplementation("io.kotest:kotest-runner-junit5:4.2.2")
     testImplementation("io.kotest:kotest-assertions-core:4.2.2")
--- src/main/kotlin/uk/co/baconi/playground/cli/Application.kt
+++ src/main/kotlin/uk/co/baconi/playground/cli/Application.kt
@@ -1,12 +1,67 @@
 package uk.co.baconi.playground.cli
 
-import org.slf4j.LoggerFactory
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.core.subcommands
+import com.github.ajalt.clikt.parameters.options.default
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.types.int
+import com.github.ajalt.clikt.parameters.types.restrictTo
+import kotlin.random.Random
 
-private val logger = LoggerFactory.getLogger("uk.co.baconi.playground.cli.Application")
+class Application : CliktCommand(printHelpOnEmptyArgs = true) {
+    override fun run() {}
+}
 
-/**
- * Entry point of the application
- */
+class Create : CliktCommand() {
+
+    companion object {
+        private val charPool: List<Char> by lazy { ('a'..'z') + ('A'..'Z') + ('0'..'9') }
+
+        private val randomPrefix: String by lazy { randomString(4) }
+
+        private fun randomString(length: Int) = (1..length)
+            .map { Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+    }
+
+    private val username by option("-u", "--username", help = "Username to create account with")
+        .default(randomString(8))
+        .validate {
+            require(it.length >= 8) { "must be 8 characters long" }
+            require(it.contains("[a-zA-Z0-9]".toRegex())) { "must consist of only alpha numeric characters" }
+        }
+
+    private val pin by option("-p", "--pin", help = "6 digit pin to create account with")
+        .int()
+        .restrictTo(100000..999999)
+        .default(121212)
+
+    private val prefix by option("--prefix", help = "Prefix for generated fields")
+        .default(randomPrefix)
+        .validate {
+            require(it.contains("[a-zA-Z0-9]".toRegex())) { "must consist of only alpha numeric characters" }
+        }
+
+    override fun run() {
+        echo("Username: $username")
+        echo("Pin: $pin")
+        echo("Prefix: $prefix")
+    }
+}
+
+class Update : CliktCommand() {
+    override fun run() {
+        echo("I would update something")
+    }
+}
+
 fun main(args: Array<String>) {
-  logger.info("Hello World: {}", args)
+    Application()
+        .subcommands(
+          Create(),
+          Update()
+        )
+        .main(args)
 }
